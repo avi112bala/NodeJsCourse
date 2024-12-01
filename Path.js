b1eeedfeba075381aca24c8a2c1b11272075d401
Avi112bala <avinash20802bala@gmail.com>
2024-12-01 22:03:32 +0530

    latestpush

---
+++ Path.js
@@ -0,0 +1,12 @@
+const path =require("path")
+
+// let ext = path.extname("C:UsersAvinash mishraOneDriveDesktopScalarnodesample.txt");
+// let ext = path.extname("./sample.txt");
+// let basename=path.basename("./sample.txt")
+// console.log(ext);
+// console.log(basename);
+
+// console.log(__filename);
+// console.log(__dirname);
+
+
---
+++ index.js
@@ -0,0 +1,9 @@
+// const calculator=require("./cals")
+// calculator.addtion(5,5)
+
+//child Process
+
+// let cp =require("child_process");
+// cp.execSync("calc");
+// cp.execSync("start chrome")
+// cp.execSync("start chrome https://avinash112.vercel.app")
